1184a3ad176bf2a3b012e0c725741f074386ac45
CGNO1210 <cgno1210@gmail.com>
2024-04-06 13:45:30 +0700

    db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -23,6 +23,7 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME
 
 let connectDB = async () => {
   try {
+    console.log(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD);
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
